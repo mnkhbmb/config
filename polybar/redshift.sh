53a38124e1469eebccb4fd390b828056e388d125
Mnkhbmb <munkhbymba0217@gmail.com>
2018-01-11 16:26:23 +0800

    xdefaults added

--- polybar/redshift.sh
+++ polybar/redshift.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Specifying the icon(s) in the script
-# This allows us to change its appearance conditionally
-icon=""
+if [ "$(pgrep -x redshift)" ]; then
+    temp=$(redshift -p 2> /dev/null | grep temp | cut -d " " -f 3 | tr -d "[:alpha:]")
 
-pgrep -x redshift &> /dev/null
-if [[ $? -eq 0 ]]; then
-    temp=$(redshift -p 2>/dev/null | grep temp | cut -d' ' -f3)
-    temp=${temp//K/}
-fi
-
-# OPTIONAL: Append ' ${temp}K' after $icon
-if [[ -z $temp ]]; then
-    echo "%{F#65737E}$icon ${temp}K"       # Greyed out (not running)
-elif [[ $temp -ge 5000 ]]; then
-    echo "%{F#8FA1B3}$icon ${temp}K"       # Blue
-elif [[ $temp -ge 4000 ]]; then
-    echo "%{F#EBCB8B}$icon ${temp}K"       # Yellow
-else
-    echo "%{F#D08770}$icon ${temp}K"       # Orange
+    if [ -z $temp ]; then
+        echo "%{F#65737E} #"
+    elif [ $temp -ge 5000 ]; then
+        echo "%{F#8FA1B3} #"
+    elif [ $temp -ge 4000 ]; then
+        echo "%{F#EBCB8B} #"
+    else
+        echo "%{F#D08770} #"
+    fi
 fi
---
+++ polybar/usbmount.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
+
+case "$1" in
+    --mount)
+        for mount in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+            udisksctl mount --no-user-interaction -b $mount
+
+            # mountpoint=$(udisksctl mount --no-user-interaction -b $mount)
+            # mountpoint=$(echo $mountpoint | cut -d " " -f 4 | tr -d ".")
+            # terminal -e "bash -lc 'filemanager $mountpoint'"
+        done
+        ;;
+    --unmount)
+        for unmount in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .name'); do
+            udisksctl unmount --no-user-interaction -b $unmount
+            udisksctl power-off --no-user-interaction -b $unmount
+        done
+        ;;
+    *)
+        output=""
+        for unmounted in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+            unmounted=$(echo $unmounted | tr -d "[:digit:]")
+            unmounted=$(echo $devices | jq -r '.blockdevices[]  | select(.name == "'$unmounted'") | .vendor')
+            unmounted=$(echo $unmounted | tr -d ' ')
+
+            output="$output# $unmounted   "
+        done
+
+        for mounted in $(echo $devices | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .size'); do
+            output="$output# $mounted   "
+        done
+
+        echo "$output"
+        ;;
+esac
